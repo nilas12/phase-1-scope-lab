572e30db148f827f796cb2f73b9cf8916d042ca4
Your Name <senasezer@sena-MacBook-Air.local>
2022-10-26 00:19:55 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+  }
+  function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+  }
+  const leastCustomer = "bob"
+  function changeLeastFavoriteCustomer() {
+    return leastCustomer = 'someelse'
+  }
